a607f0a084dbf599c2b2d80c846a867f3a48770c
bolives-hax <125610892+bolives-hax@users.noreply.github.com>
2024-09-05 23:22:20 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,8 +3,8 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
   outputs = { self, nixpkgs, ... }: {
-    packages.x86_64-linux = {
-      ...
+    packages.x86_64-linux = let pkgs = import (nixpkgs {system = "x86_64-linux"; }); in
+      pkgs;
     };
 
     hydraJobs = {
