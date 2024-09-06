2f6cca6011b8826e08824dbe2ee65d1a87250b22
bl0v3 <bl0v3@bl0v3.mailme.at.bl0v3_followed_by_dot_and_com>
2024-09-06 15:44:27 +0200

    fix

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
   outputs = { self, nixpkgs, ... }: {
-    packages.x86_64-linux = let pkgs = import (nixpkgs {system = "x86_64-linux"; }); in
+    packages.x86_64-linux = let pkgs = import nixpkgs {system = "x86_64-linux"; }; in
       pkgs;
 
     hydraJobs = {
